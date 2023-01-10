395377756ee0d898dcda6c4ffa05469dc9e70111
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-01-10 10:08:31 +0100

    Ejercicio3

---
+++ 00.IntroducciónPython/03.EjercicioCadenas.py
@@ -0,0 +1,7 @@
+# Muestre tres cadenas "Nombre", "Es", "Ignatius" como "Nombre ** Es ** Ignatius"
+# Utilice la función print() para formatear las palabras dadas en el formato mencionado. 
+# Muestre el separador ** entre cada cadena.
+
+print("Nombre", end=" ** ")
+print("Es", end=" ** ")
+print("Ignatius")
