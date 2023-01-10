412594ccbcb096beb5d5b02340ff574852af0f74
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-01-10 10:02:15 +0100

    Ejercicio1

---
+++ 00.IntroducciónPython/01.EjercicioMultiplicación.py
@@ -0,0 +1,4 @@
+# Escriba un programa para aceptar dos números del usuario y calcular la multiplicación
+numero1 = int(input("Introduzca un número: "))
+numero2 = int(input("Introduzca otro número: "))
+print("Resultado de la multiplicación:", numero1,"x", numero2, "=", numero1*numero2)
