75163cea2c3dc97af1533b76c734c4a20d7f41ae
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-01-10 10:21:43 +0100

    Ejercicio5

--- 00.IntroducciónPython/02.EjercicioStringFormat.py
+++ 00.IntroducciónPython/02.EjercicioStringFormat.py
@@ -3,5 +3,5 @@
 # para formatear las siguientes tres variables según la salida esperada.
 # Resultado: Tengo 1000 euros para comprar 3 tarjetas gráficas por 450,00 dólares.
 
-resultado = "Tengo {totalMoney:} euros para comprar {quantity} tarjetas gráficas por {price:.2f} dólares."
+resultado = "Tengo {totalMoney} euros para comprar {quantity} tarjetas gráficas por {price:.2f} dólares."
 print(resultado.format(totalMoney = 1000, quantity = 3, price = 450))
---
+++ 00.IntroducciónPython/05.EjercicioDecimal.py
@@ -0,0 +1,5 @@
+# Muestre el número flotante con 2 lugares decimales usando print()
+# Dado: num = 458.541315
+# Resultado: 458,54
+num = 458.541315
+print(f"{num:.2f}")
